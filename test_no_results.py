4e8357245b0256033b0bd545e7984423a624e0c3
Denis <denisgaldin@mail.ru>
2024-09-23 22:07:47 +0300

    Тест на поиск, который проверит, что поиск не выдает результатов

--- test_no_results.py
+++ test_no_results.py
@@ -3,5 +3,5 @@ from selene import be, have
 
 def test_no_results(setup_browser):
     setup_browser.element('[name="q"]').should(be.blank).type('sdfajsdfadkjf').press_enter()
-    setup_browser.element('[data-testid=web-vertical]').should(
+    setup_browser.element('[data-testid="web-vertical"]').should(
         have.text('По запросу sdfajsdfadkjf результаты не найдены.'))
