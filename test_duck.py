3003713975964afca3aa452f4c47e340010e32ce
Denis <denisgaldin@mail.ru>
2024-09-23 21:49:54 +0300

    Тест на поиск, который проверит, что поиск не выдает результатов

--- test_duck.py
+++ test_duck.py
@@ -1,12 +1,14 @@
 from selene import browser, be, have
 import pytest
 
+
 @pytest.fixture
 def setup_browser():
     browser.open('https://duckduckgo.com/')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     return browser
 
-def test_selene(setup_browser):
-    assert setup_browser.element('[data-testid=web-vertical]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
+def test_selene(setup_browser):
+    assert setup_browser.element('[data-testid=web-vertical]').should(
+        have.text('Selene - User-oriented Web UI browser tests in Python'))
--- test_no_results.py
+++ test_no_results.py
@@ -1,7 +1,7 @@
-import pytest
-from selene import browser, be, have, by
+from selene import be, have
 
 
 def test_no_results(setup_browser):
     setup_browser.element('[name="q"]').should(be.blank).type('sdfajsdfadkjf').press_enter()
-    setup_browser.element('[data-testid=web-vertical]').should(have.text('По запросу sdfajsdfadkjf результаты не найдены.'))
+    setup_browser.element('[data-testid=web-vertical]').should(
+        have.text('По запросу sdfajsdfadkjf результаты не найдены.'))
