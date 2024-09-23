f72bd49f4b374a855efce403b7d1cce75bd328e8
Denis <denisgaldin@mail.ru>
2024-09-23 22:09:59 +0300

    Тест на поиск, который проверит, что поиск не выдает результатов

--- test_duck.py
+++ test_duck.py
@@ -4,11 +4,16 @@ import pytest
 
 @pytest.fixture
 def setup_browser():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     browser.open('https://duckduckgo.com/')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    return browser
+
+    yield browser
+    browser.quit()
 
 
 def test_selene(setup_browser):
-    assert setup_browser.element('[data-testid=web-vertical]').should(
+    setup_browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+
+    setup_browser.element('[data-testid="web-vertical"]').should(
         have.text('Selene - User-oriented Web UI browser tests in Python'))
