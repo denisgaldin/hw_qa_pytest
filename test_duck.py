8579fbd8a8309be22ee2ebda08f55ae1aef38275
Denis <denisgaldin@mail.ru>
2024-09-24 00:18:45 +0300

    Измененный первый автотест

--- test_duck.py
+++ test_duck.py
@@ -1,19 +1,18 @@
-from selene import browser, be, have
 import pytest
+from selene import browser, be, have
 
 
-@pytest.fixture
+@pytest.fixture(scope='function', autouse=True)
 def setup_browser():
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.open('https://duckduckgo.com/')
-
-    yield browser
+    browser.config.browser_name = 'chrome'
+    browser.config.base_url = 'https://duckduckgo.com'
+    browser.config.timeout = 10
+    yield
     browser.quit()
 
 
-def test_selene(setup_browser):
-    setup_browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-
-    setup_browser.element('[data-testid="web-vertical"]').should(
+def test_duckduckgo_search():
+    browser.open('https://duckduckgo.com/')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element("[data-testid=web-vertical]").should(
         have.text('Selene - User-oriented Web UI browser tests in Python'))
